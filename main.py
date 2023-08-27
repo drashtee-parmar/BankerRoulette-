3df0082aa60413fc181e9a47cef05de58d3ae1ad
Drashtee Parmar <drashteeparmar@gmail.com>
2023-08-26 22:19:17 -0400

    Create main with random generator

---
+++ main.py
@@ -0,0 +1,15 @@
+# Import the random module here
+import random
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+# print(len(names))
+num_items = len(names)
+
+random_choice = random.randint(0, num_items - 1)
+person_who_will_pay = names[random_choice]
+print(person_who_will_pay + " is going to buy the meal today!")
