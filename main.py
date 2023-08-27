46f0cd37547b6a87a07ef22b9a67e7c5d8229c1f
Drashtee Parmar <drashteeparmar@gmail.com>
2023-08-26 22:22:12 -0400

    Update main using random.choice()

--- main.py
+++ main.py
@@ -13,3 +13,9 @@ num_items = len(names)
 random_choice = random.randint(0, num_items - 1)
 person_who_will_pay = names[random_choice]
 print(person_who_will_pay + " is going to buy the meal today!")
+
+##### using choice() #####
+# person_who_will_pay = random.choice(names)
+
+
+# print(person_who_will_pay + " is going to buy the meal today!")
